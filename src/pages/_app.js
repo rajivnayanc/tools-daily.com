5a8eb1b1f5b26f29e7c4498d4c76cacd1f7d418e
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2025-06-20 19:30:02 +0530

    Load the fontawesome css in the _app.js to avoid bigger fonts while loading the page.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layout'
 import '../globals.css'
-
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Layout><Component {...pageProps} /></Layout>
